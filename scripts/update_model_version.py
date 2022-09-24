f871b39d069f04ca463148855bd2eba13e0f2245
ddl-srinivap <98163257+ddl-srinivap@users.noreply.github.com>
2022-09-23 22:11:21 -0400

    Update update_model_version.py

--- scripts/update_model_version.py
+++ scripts/update_model_version.py
@@ -3,10 +3,9 @@ import mlflow
 import json
 import sys
 
-domino_api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21pbm9fYXBpX2tleSI6IjM3MDIyODEzYmVlZjIwNzUyMDQ0ZDFjMDU3MTI1ODc1MjRjZWMxZGY3MmEyY2ZiOGM1NTg4MjkzYjM4YWZhNjQiLCJkb21pbm9fcHJvamVjdF9uYW1lIjoibW9kZWwtZGVwbG95bWVudCIsImRvbWlub19ydW5faWQiOiJtb2RlbC1kZXBsb3llciIsInRhZ3MiOnt9fQ.4GfUjj5O7HDgrrbpKoh-RPCyEgG8gnAN1KN11oCoKB8"
-os.environ["MLFLOW_TRACKING_URI"] = "https://fsec.cs.domino.tech/mlflow-efs/#"
-os.environ["MLFLOW_TRACKING_TOKEN"] = domino_api_key   
-client = mlflow.tracking.client.MlflowClient()
+os.environ["MLFLOW_TRACKING_URI"] = "https://fsec.cs.domino.tech/mlflow-efs/"
+os.environ["MLFLOW_TRACKING_TOKEN"] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21pbm9fYXBpX2tleSI6IjE3ODU2MjA5NTg4NDg4ZTgiLCJkb21pbm9fcHJvamVjdF9uYW1lIjoibWxmbG93LW1sb3BzLWRlbW8iLCJkb21pbm9fcnVuX2lkIjoiLSIsInRhZ3MiOnsiZG9taW5vX3Byb2plY3RfaWQiOiI2MzE4Y2ZmOWY0ZDMwMTM0NGZmMGMxMWYifX0.iYCfOOjKWPYu9Gq3Vc0Kka6-ZZOpWJH37wYqjUfxNNI"   
+client = mlflow.tracking.MlflowClient()
 
 with open(sys.argv[1], "r") as mv_file:
     model_versions = json.load(mv_file)
