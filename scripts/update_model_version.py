a3bd32768715f0f62431d62c42c80f60a0e68292
Pattabi Srinivasan <pattabisrinivasan@macbook-pro.myfiosgateway.com>
2022-11-16 09:17:04 -0500

    Associate model version with project id and corresponding token

--- scripts/update_model_version.py
+++ scripts/update_model_version.py
@@ -4,14 +4,19 @@ import json
 import sys
 
 os.environ["MLFLOW_TRACKING_URI"] = "https://fsec.cs.domino.tech/mlflow-efs/"
-domino_api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21pbm9fYXBpX2tleSI6IjM3MDIyODEzYmVlZjIwNzUyMDQ0ZDFjMDU3MTI1ODc1MjRjZWMxZGY3MmEyY2ZiOGM1NTg4MjkzYjM4YWZhNjQiLCJkb21pbm9fcHJvamVjdF9uYW1lIjoibW9kZWwtZGVwbG95bWVudCIsImRvbWlub19ydW5faWQiOiJtb2RlbC1kZXBsb3llciIsInRhZ3MiOnt9fQ.4GfUjj5O7HDgrrbpKoh-RPCyEgG8gnAN1KN11oCoKB8"
-#domino_api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21pbm9fYXBpX2tleSI6IjE3ODU2MjA5NTg4NDg4ZTgiLCJkb21pbm9fcHJvamVjdF9uYW1lIjoibWxmbG93LW1sb3BzLWRlbW8iLCJkb21pbm9fcnVuX2lkIjoiLSIsInRhZ3MiOnsiZG9taW5vX3Byb2plY3RfaWQiOiI2MzE4Y2ZmOWY0ZDMwMTM0NGZmMGMxMWYifX0.iYCfOOjKWPYu9Gq3Vc0Kka6-ZZOpWJH37wYqjUfxNNI"   
-os.environ["MLFLOW_TRACKING_TOKEN"] = domino_api_key
 client = mlflow.tracking.MlflowClient()
+tokens = json.loads(os.getenv("PROJECT_TOKENS"))
 
 with open(sys.argv[1], "r") as mv_file:
     model_versions = json.load(mv_file)
-    for mv in model_versions:
-        client.transition_model_version_stage(name = mv["name"],
-                        version = mv["version"],
-                        stage = mv["stage"])
+    for project_id in model_versions:
+        tracking_token = tokens[f"PROJECT_ID_{project_id}"]
+        if not tracking_token:
+            raise Exception(f"Toekn not found for project id {project_id}")
+        os.environ["MLFLOW_TRACKING_TOKEN"] = tracking_token
+        for name_ver in model_versions[project_id]:
+            client.transition_model_version_stage(
+                name=name_ver["name"],
+                version=str(name_ver["version"]),
+                stage="Production",
+            )
